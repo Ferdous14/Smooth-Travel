d442cceb6b7930073e1eb1ed6c3be98c7d8b840f
Ferdous Ahmed <ferdous4131@gmail.com>
2024-02-19 19:40:15 +0600

    added the modal

--- js/index.js
+++ js/index.js
@@ -3,12 +3,13 @@ let count = 0;
 const selectedSeat = 1;
 const ticketPrice = 550;
 let sum = 0;
-const classNmae = 'Economy';
+const className = 'Economy';
+
+
 for (const seat of allSeats) {
     seat.addEventListener("click", function () {
-
         if (count >= 4) {
-            alert("This is an alert message!");
+            alert("You have selected maximum seat!");
             return;
         } else {
             // seat count
@@ -16,8 +17,23 @@ for (const seat of allSeats) {
             const seatCount = document.getElementById('seat-count');
             seatCount.innerText = count;
 
+            // append seat
+            const dynamicList = document.getElementById('dynamic-list');
+            const p = document.createElement('p');
+            const seatText = seat.innerText;
+            p.innerText = seatText;
+            dynamicList.appendChild(p)
+
+            const p1 = document.createElement('p');
+            p1.innerText = className;
+            dynamicList.appendChild(p1)
+
+            const p2 = document.createElement('p');
+            p2.innerText = ticketPrice;
+            dynamicList.appendChild(p2)
+
             // background color change
-            seat.classList.add('bg-[#1DD100]')
+            seat.classList.add('bg-[#1DD100]');
 
             // total seat count
             const totalSeatElement = document.getElementById('total-seat');
@@ -35,35 +51,42 @@ for (const seat of allSeats) {
             grandTotalElement.innerText = sum;
 
             // coupon
-
-            if (count >= 4) {
-                const applyBtn = document.getElementById('apply-btn').removeAttribute('disabled');
-
+            if (count === 4) {
+                document.getElementById('apply-btn').removeAttribute('disabled');
             }
-            const inputField = document.getElementById('coupon');
-            const applyBtn = document.getElementById('apply-btn');
-            applyBtn.addEventListener('click', function () {
-                const couponCode = inputField.value;
-                if (couponCode === 'New15') {
-                    const discount = sum * 0.15;
-                    const newGrandTotal = sum - discount;
-                    grandTotalElement.innerText = newGrandTotal;
-                    applyBtn.disabled = true;
-                } else if (couponCode === 'Couple 20') {
-                    const newDiscount = sum * 0.20;
-                    const finalGrandTotal = sum - newDiscount;
-                    grandTotalElement.innerText = finalGrandTotal;
-                    applyBtn.disabled = true;
-                }else{
-                    alert('Invalid Cupon')
-                }
-            })
-
         }
-
-
-    })
+    });
 }
 
+
+
+const applyBtn = document.getElementById('apply-btn');
+applyBtn.addEventListener('click', function () {
+    const inputField = document.getElementById('coupon');
+    const couponCode = inputField.value;
+    const grandTotalElement = document.getElementById('grand-total');
+    if (couponCode === 'New15') {
+        const discount = sum * 0.15;
+        const newGrandTotal = sum - discount;
+        grandTotalElement.innerText = newGrandTotal;
+        inputField.setAttribute('hidden', true);
+        applyBtn.setAttribute('hidden', true);
+    } else if (couponCode === 'Couple 20') {
+        const newDiscount = sum * 0.20;
+        const finalGrandTotal = sum - newDiscount;
+        grandTotalElement.innerText = finalGrandTotal;
+        applyBtn.setAttribute('hidden', true);
+        inputField.setAttribute('hidden', true);
+    } else {
+        alert('Invalid Cupon')
+    }
+})
+
+const btnModal = document.getElementById('btn-modal');
+const modal = document.getElementById('my_modal_5');
+
+btnModal.onclick = function () {
+    modal.showModal();
+}
 
 
