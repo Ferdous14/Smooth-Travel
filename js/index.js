7369cd4503a85ba135e1ef3c7673ab1ec04fa0cf
Ferdous Ahmed <ferdous4131@gmail.com>
2024-02-19 10:58:36 +0600

    Added the js file and completed some requirements

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-console.log("hi");
+const allSeats = document.getElementsByClassName('Seat');
+let count = 0;
+const selectedSeat = 1;
+for (const seat of allSeats){
+    seat.addEventListener("click", function(){
+        // background color change
+        seat.classList.add('bg-[#1DD100]')
+        // seat count
+        count = count+1;
+        const seatCount = document.getElementById('seat-count');
+        seatCount.innerText = count;
+        // total seat count
+        const totalSeatElement = document.getElementById('total-seat');
+        const totalSeat = parseInt(totalSeatElement.innerText);
+        const remainingSeat = totalSeat - selectedSeat;
+        totalSeatElement.innerText = remainingSeat;
+        
+    })
+}
+
